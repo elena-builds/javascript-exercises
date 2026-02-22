9eceba4dd3a316280766458aba77791ef0e2022b
Elena <260600984+elena-builds@users.noreply.github.com>
2026-02-22 20:34:31 +0800

    Complete reversestring exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let result = "";
 
+  for (let i = 0; i < string.length; i++) {
+    result += string[string.length - (i + 1)];
+  }
+
+  return result;
 };
 
 // Do not edit below this line
