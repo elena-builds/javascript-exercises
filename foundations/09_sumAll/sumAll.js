5a92c387493596e3ac371ed5112ba66ae57ce445
Elena <260600984+elena-builds@users.noreply.github.com>
2026-02-22 22:25:47 +0800

    Complete sumall exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let arr = [a, b];
+  arr.sort((a, b) => a - b);
+  let c = arr[0];
+  let d = arr[1];
+  let sum = c + d;
 
+  if (
+    c < 0 ||
+    d < 0 ||
+    typeof c !== "number" ||
+    typeof d !== "number" ||
+    !Number.isInteger(c) ||
+    !Number.isInteger(d)
+  ) {
+    return "ERROR";
+  }
+
+  for (let i = c + 1; i < d; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
