757a4b4795a9966de994e984e0fa01da974406a0
Elena <260600984+elena-builds@users.noreply.github.com>
2026-02-26 12:20:45 +0800

    Complete fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function fibonacci(num) {
+  num = Number(num);
 
+  if (num < 0) return "OOPS";
+  if (num === 0) return 0;
+  if (num === 1) return 1;
+
+  let previous = 0;
+  let current = 1;
+
+  for (let i = 2; i <= num; i++) {
+    const next = previous + current;
+    previous = current;
+    current = next;
+  }
+  return current;
 };
 
 // Do not edit below this line
