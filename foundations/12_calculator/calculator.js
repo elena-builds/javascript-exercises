91cc20cc8d633063aa6e19fddf77aa72f35660cd
Elena <260600984+elena-builds@users.noreply.github.com>
2026-02-26 10:48:57 +0800

    Complete caculator exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const sum = function sum(array) {
+  return array.reduce((sum, num) => sum + num, 0);
 };
 
-const subtract = function() {
-	
+const multiply = function multiply(array) {
+  return array.reduce((result, num) => result * num, 1);
 };
 
-const sum = function() {
-	
-};
+const power = (a, b) => a ** b;
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function factorial(num) {
+  if (num > 0) {
+    let result = 1;
+    for (let i = 1; i <= num; i++) {
+      result = result * i;
+    }
+    return result;
+  }
+  return 1;
 };
 
 // Do not edit below this line
@@ -29,5 +30,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
