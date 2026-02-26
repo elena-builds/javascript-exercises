fc639c384a635f8a03d08a10754cbf9181435952
Elena <260600984+elena-builds@users.noreply.github.com>
2026-02-26 11:27:38 +0800

    Complete palindromes exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function palindromes(originalStr) {
+  let cleanedStr = originalStr.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+  let reversedStr = "";
+  for (let i = cleanedStr.length - 1; i >= 0; i--) {
+    reversedStr += cleanedStr[i];
+  }
+
+  return cleanedStr === reversedStr;
 };
 
 // Do not edit below this line
