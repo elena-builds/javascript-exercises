8ca32280de97c137996dae85dc3e23744d3fbe07
Elena <260600984+elena-builds@users.noreply.github.com>
2026-02-26 13:24:29 +0800

    Complete find the oldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  array.sort(function (a, b) {
+    if (a.yearOfDeath === undefined) {
+      a.yearOfDeath = new Date().getFullYear();
+    }
+    if (b.yearOfDeath === undefined) {
+      b.yearOfDeath = new Date().getFullYear();
+    }
+    const personA = a.yearOfDeath - a.yearOfBirth;
+    const personB = b.yearOfDeath - b.yearOfBirth;
 
+    return personA > personB ? -1 : 1;
+  });
+  return array[0];
 };
 
 // Do not edit below this line
